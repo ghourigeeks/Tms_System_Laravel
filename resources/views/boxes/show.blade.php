cf06d428d5258755789549b54211be728f58f35d
aquib20034 <aquib20034@gmail.com>
2022-07-29 19:57:26 +0500

    udpated

--- resources/views/boxes/show.blade.php
+++ resources/views/boxes/show.blade.php
@@ -12,74 +12,46 @@
                 <div class="card">
                     <div class="card-header">
                         <div class="d-flex align-items-center">
-                            <h4 class="card-title">Show @yield('title')</h4>
-                            <a  href="{{ route('boxes.index') }}" class="btn btn-primary btn-xs ml-auto">
-                                <i class="fas fa-arrow-left"></i>
-                            </a>
+                            <h4 class="card-title">@yield('title') box</h4>
+                            <span class="ml-auto">
+                                @if((isset($data->active)) && ( ($data->active == 1) || ($data->active == "Active") ) )
+                                    <span class="badge badge-success">Active</span>
+                                @else
+                                    <span class="badge badge-danger">Inactive</span>
+                                @endif
+                            </span>
                         </div>
                     </div>
-                    
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-12 col-md-12">
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                 <div class="table-responsive">
                                     <table class="table dt-responsive">
                                         <tbody>
-
                                             <tr>
-                                                <td width="30%">Client name</td>
-                                                <td>
-                                                        {{ isset($data->client->fullname) ? ($data->client->fullname) : ""}}
-                                                </td>
+                                                <th width="30%">Box name</th>
+                                                <td>{{$data->name}}</td>
+                                                <th width="30%">QR code</th>
+                                                <td>{{ isset($data->qrcode) ? ($data->qrcode) : ""}}</td>
                                             </tr>
 
                                             <tr>
-                                                <td width="30%">Box name</td>
-                                                <td>
-                                                        {{ isset($data->name) ? ($data->name) : ""}}
-                                                </td>
+                                                <th>Price</th>
+                                                <td>{{ isset($data->price) ? ($data->price) : ""}}</td>
+                                                <th>Barcode</th>
+                                                <td>{{ isset($data->barcode) ? ($data->barcode) : ""}}</td>
                                             </tr>
+
                                             <tr>
-                                                <td width="30%">Price</td>
-                                                <td>
-                                                        {{ isset($data->price) ? ($data->price) : ""}}
-                                                </td>
+                                                <th>Description</th>
+                                                <td colspan="3">{{ isset($data->description) ? ($data->description) : ""}}</td>
                                             </tr>
-                                            <tr>
-                                                <td width="30%">QR code</td>
-                                                <td>
-                                                        {{ isset($data->qrcode) ? ($data->qrcode) : ""}}
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td width="30%">Bar code</td>
-                                                <td>
-                                                        {{ isset($data->barcode) ? ($data->barcode) : ""}}
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td width="30%">Descripion</td>
-                                                <td>
-                                                        {{ isset($data->description) ? ($data->description) : ""}}
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>Status</td>
-                                                <td>
-                                                
-                                                    @if((isset($data->active)) && ( ($data->active == 1) || ($data->active == "Active") ) )
-                                                        <span class="badge badge-success">Active</span>
-                                                    @else
-                                                        <span class="badge badge-danger">Inactive</span>
-                                                    @endif
-                                                </td>
-                                            </tr>
-                                            
-                                           
+                                          
                                         </tbody>
                                     </table>
                                 </div>
                             </div>
+                        
                         </div>
                     </div>
                 </div>
