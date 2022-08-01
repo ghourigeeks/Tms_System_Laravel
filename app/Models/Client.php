52e7b44b14116281a07057389e958f722355f29d
Basim Ghouri <97091625+ghourigeeks@users.noreply.github.com>
2022-08-01 17:11:15 +0500

    Update Client.php

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -79,4 +79,9 @@ class Client extends Authenticatable
     {
         return $this->belongsTo(Country::class,'country_id','id');
     }
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class, 'client_id', 'id');
+    }
 }
