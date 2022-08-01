1f9b1deaf64a6705d52c53e82c2f027742d7b310
Basim Ghouri <97091625+ghourigeeks@users.noreply.github.com>
2022-08-01 17:09:39 +0500

    Create complaints.blade.php

---
+++ resources/views/clients/complaints.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.main')
+@section('title','Client')
+@section('content')
+
+@include( '../sweet_script')
+<div class="page-inner">
+    <div class="page-header">
+        <h4 class="page-title">@yield('title'): {{ isset($data->client->fullname) ? ($data->client->fullname) : ""}}</h4>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h4 class="card-title"> @yield('title') Complaint devices </h4>
+                        <a  href="{{ url('clients')}}/{{$id}}" class="btn btn-primary btn-xs ml-auto">
+                            <i class="fas fa-arrow-left"></i>
+                        </a>
+                    </div>
+                    
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table id="myTable" class="table table-hover" style="width: 100%;" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th width="5%">#</th>
+                                    <th>Client name#</th>
+                                    <th>Subject</th>
+                                    <th>Complaint</th>
+                                    <th width="10%" >Action</th>
+                                </tr>
+                            </thead>
+                            <tbody></tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    $(document).ready(function () {  
+        var t = $('#myTable').DataTable({
+                "aaSorting": [],
+                "processing": true,
+                "serverSide": false,
+                "select":true,
+                "ajax": "{{ url('clients') }}/complaints_lst/{{$id}}",
+                "method": "GET",
+                "columns": [
+                    {"data": "srno"},
+                    {"data": "client_id"},
+                    {"data": "subject"},
+                    {"data": "complaint"},
+                    {"data": "action",orderable:false,searchable:false}
+
+                ]
+            });
+        t.on( 'order.dt search.dt', function () {
+            t.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+                cell.innerHTML = i+1;
+            } );
+        } ).draw();
+    });
+</script>
+
+@endsection
